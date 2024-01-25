b123bc753a83298f7cb34b5fb2eba3e1d31c2e9d
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 12:05:29 +0800

    final

--- return_the_count_of_a_given_substring_from_a_string.py
+++ return_the_count_of_a_given_substring_from_a_string.py
@@ -1,7 +1,16 @@
 # Return the count of a given substring from a string
 
 def count_substring(string, substring):
-    return string.count(substring)
+    count = 0
+    start = 0
+    while start < len(string):
+        pos = string.find(substring, start)
+        if pos != -1:
+            start = pos + 1
+            count += 1
+        else:
+            break
+    return count
 
 str_x = "Emma is good developer. Emma is a writer"
 substring = "Emma"
